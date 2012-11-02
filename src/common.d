9cdbe7261fed99d3dfcbbdbe1bf8708b87405cba
Dimitri Sabadie <sabadie.dimitri@gmail.com>
2012-11-02 16:52:30 +0100

    Update src/common.d
    Added my own any function. I’m gonna open an issue on dsource about why I did such a thing.

--- src/common.d
+++ src/common.d
@@ -1,24 +1,22 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   sdb, the Simple D Builder
-   Copyright (C) 2012 Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com> 
+    sdb, the Simple D Builder
+    Copyright (C) 2012 Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com> 
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 module common;
-
-/* This module gathers all the fucking common stuff. */
 
 import std.array : replace;
 
@@ -37,8 +35,22 @@ string module_to_file(string m, string rootsrc) {
      * the corresponding file is:
      *   $(ROOT_SRC)/foo/bar/test/final/math/matrix.d
      */
-
+    
     m = m.replace(".", "/");
     m ~= ".d";
     return rootsrc ~ '/' ~ m;
+}
+
+/* A linear search function. */
+bool any(alias __Pred, __Range)(__Range r) {
+    foreach (a; r) {
+        static if (is(typeof(__Pred) : string)) {
+            mixin("return (" ~ __Pred ~ ");");
+        } else {
+            if (__Pred(a))
+                return true;
+        }
+    }
+    
+    return false;
 }
